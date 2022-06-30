c116619af34a98e84b19f5cda034b3e538802338
guilherme <ext.pablo.cunha@motz.com.br>
2022-06-30 12:48:27 -0300

    Initial Commit

---
+++ BernaViagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BernaViagens
+//
+//  Created by Guilherme Bernava on 30/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
